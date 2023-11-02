088d2fbe9298d12448c0e99d5ec4c0513a99f522
Alexby32 <99358945+Alexby32@users.noreply.github.com>
2023-11-01 22:35:14 -0500

    ProyectoPV

---
+++ src/app/web/products/products.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    const response = this.httpClient.get("https://api.escuelajs.co/api/v1/products").subscribe(
+      Responde => {
+        console.log(response);
+      }
+    )
+  }
+
+}
